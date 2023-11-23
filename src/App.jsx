9cc1e1a13e31d396065314b8f2512b8182125a29
larissajuliavsa <larissa.jvsa@gmail.com>
2023-11-23 12:35:19 -0300

    feat: add index, app, utils, and style config

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,20 @@
-import React, { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import './App.css';
+/* eslint-disable import/no-extraneous-dependencies */
+// import React, { useState } from 'react';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import Veiculos from './Pages/Veiculos';
+import Product from './Pages/Product';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="submit" onClick={() => setCount((counter) => counter + 1)}>
-          count is
-          {' '}
-          {count}
-        </button>
-        <p>
-          Edit
-          {' '}
-          <code>src/App.jsx</code>
-          {' '}
-          and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/veiculos" element={<Veiculos />} />
+        <Route path="/product" element={<Product />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
